eb7eb55e023aa5c01b095d647c803f5e5fe56885
Jacktrooper <jack.corso@flemingcollege.ca>
2023-01-18 14:03:51 -0500

    changed both script1 and 2
    added a line to script2 and deleted a line of script1

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #This is cool
 print ("If the implementation is hard to explain, it's a bad idea.")
 print ("If the implementation is easy to explain, it may be a good idea.")
+print ("Although that way may not be obvious at first unless you're Dutch.")
